6bb9104d1b522582dac3e96da263f7b28f4a8b54
Andres17X <101605263+Andres17X@users.noreply.github.com>
2022-03-15 12:15:02 +0100

    Update discod.java

---
+++ src/proyectogit/discod.java
@@ -0,0 +1,45 @@
+package src.proyectogit;
+import java.util.Scanner;
+
+public class Modificar extends Disco{
+    public Modificar(){
+
+    }
+    public void mod (Disco d[]){
+        System.out.println("Introduce el autor del disco");
+        int e=new Scanner(System.in).nextInt();
+        if (d[e].getEstado().equals("LIBRE")){
+            System.out.println("El disco que has seleccionado no existe, prueba con otro disco.");
+        }else{
+            System.out.println("Que es lo que quieres modificar?");
+            System.out.println("1.Autor del disco ");
+            System.out.println("2.Género del disco ");
+            System.out.println("3.Título del disco ");
+            System.out.println("4.Duración del disco ");
+            int op=new Scanner(System.in).nextInt();
+            switch (op){
+                case 1 -> {
+                    System.out.println("Introduce el autor del disco");
+                    String autor2=new Scanner(System.in).nextLine();
+                    d[e].setAutor(autor2);
+                }
+                case 2 -> {
+                    System.out.println("Introduce el genero del disco");
+                    String genero2=new Scanner(System.in).nextLine();
+                    d[e].setGenero(genero2);
+                }
+                case 3 -> {
+                    System.out.println("Introduce el título del disco");
+                    String titulo2=new Scanner(System.in).nextLine();
+                    d[e].setTitulo(titulo2);
+                }
+                case 4 -> {
+                    System.out.println("Introduce la duración ");
+                    int dur2=new Scanner(System.in).nextInt();
+                    d[e].setDuracion(dur2);
+                }
+                default -> System.out.println("Opción incorrecta :(");
+            }
+        }
+    }
+}
